3c0476582ff5ee10e76d58cb7307cb81af271f5f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-09 13:23:08 -0600

    finish list issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+};
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +24,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -55,5 +74,5 @@ var displayIssues = function(issues) {
 
 };
 
-getRepoIssues("klong75/coding-quiz-challenge");
+getRepoIssues("facebook/react");
 
